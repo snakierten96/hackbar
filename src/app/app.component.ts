41562fdcdffb3c8889df716631d97d23f67dc988
snakierten96 <neil.lunn1@gmail.com>
2017-08-15 00:02:30 +0000

    (feat) Add Fixed Topbar with scroll sensing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  scrolling = false;
+  
+  @HostListener('window:scroll',['$event']) onScroll($event) {
+    //console.log(window.scrollY);
+    if ( window.scrollY === 0 ) {
+      this.scrolling = false;
+    } else {
+      this.scrolling = true;
+    }
+  }
 }
